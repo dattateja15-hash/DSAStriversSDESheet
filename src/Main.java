e86439545add2653d1b6099fbbbf098bfa2aea4e
Datta Teja, Nalluri <dattatejan@gmail.com>
2025-03-03 13:33:58 -0600

    Pattern Two

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Patterns.patternOne();
         Patterns.patternTwo();
+        Patterns.patternThree();
         }
 }
--- src/Patterns.java
+++ src/Patterns.java
@@ -16,4 +16,13 @@ public class Patterns {
             System.out.println();
         }
     }
+
+    public static void patternThree(){
+        for(int i = 1;i<=5;i++){
+            for(int j = 1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
 }
