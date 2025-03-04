76c58cf99de2f1a8f96f5f68999430fae62de068
Datta Teja, Nalluri <dattatejan@gmail.com>
2025-03-04 08:25:53 -0600

    Pattern 13

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,6 @@ public class Main {
         Patterns.patternTen();
         Patterns.patternEleven();
         Patterns.patternTwelve();
+        Patterns.patternThirteen();
         }
 }
--- src/Patterns.java
+++ src/Patterns.java
@@ -164,4 +164,14 @@ public class Patterns {
             System.out.println();
         }
     }
+
+    public static void patternThirteen(){
+        int count = 1;
+        for(int i = 1;i<=5;i++){
+            for(int j = 1;j<=i;j++){
+                System.out.print(count+++" ");
+            }
+            System.out.println();
+        }
+    }
 }
