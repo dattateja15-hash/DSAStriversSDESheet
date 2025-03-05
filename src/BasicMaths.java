81e0459ca5bb049c4b4ee204703265b2a1ec1504
Datta Teja, Nalluri <dattatejan@gmail.com>
2025-03-05 11:51:18 -0600

    Basic Maths GCD, Armstrong

--- src/BasicMaths.java
+++ src/BasicMaths.java
@@ -29,4 +29,51 @@ public class BasicMaths {
         int n2 = reverseNumber(n1);
         return n1 == n2;
     }
+
+    public static int calculateGCD(int a,int b){
+        int n = Math.min(a,b);
+        int ans = 1;
+        for(int i = 2;i<n;i++){
+            if(a%i==0 && b%i==0){
+                ans = Math.max(ans,i);
+            }
+        }
+        return ans;
+    }
+
+    public static int claculateGCDBetter(int a,int b){
+        for(int i = Math.min(a,b);i>0;i--){
+            if(a%i==0 && b%i==0){
+                return i;
+            }
+        }
+        return 1;
+    }
+
+    public static int calculateGCDOptimal(int a,int b){
+        while(a > 0 && b > 0){
+            if(a > b){
+                a = a%b;
+            }
+            else{
+                b = b%a;
+            }
+        }
+        if(a>0)
+            return a;
+        return b;
+    }
+
+    public static boolean checkArmstrong(int n){
+        int count = String.valueOf(n).length();
+        int store = n;
+        System.out.println(count);
+        int sum = 0;
+        while(n>0){
+            int r = n%10;
+            sum = (int) (sum+Math.pow(r,count));
+            n = n/10;
+        }
+        return sum == store;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,9 @@ public class Main {
         System.out.println(BasicMaths.optimalCountNumbers(7789));
         System.out.println(BasicMaths.reverseNumber(10400));
         System.out.println(BasicMaths.checkPalindrome(1331));
+        System.out.println(BasicMaths.calculateGCD(9,12));
+        System.out.println(BasicMaths.claculateGCDBetter(9,12));
+        System.out.println(BasicMaths.calculateGCDOptimal(9,12));
+        System.out.println(BasicMaths.checkArmstrong(371));
         }
 }
