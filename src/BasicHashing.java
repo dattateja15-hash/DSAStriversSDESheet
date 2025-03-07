c2f06e022675518f46f0ad4e64c7dd40ec618760
Datta Teja, Nalluri <dattatejan@gmail.com>
2025-03-07 11:33:13 -0600

    Implementing HashMap in Java

---
+++ src/BasicHashing.java
@@ -0,0 +1,6 @@
+public class BasicHashing {
+    //Pres-storing and fetching
+
+    //Division Method = {2,5,16,28,139} divide by % of size of array (Linear Chaining)
+    //
+}
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
-
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        BasicRecursion.count(10);
-        BasicRecursion.printNameNtimes(10,1);
-        BasicRecursion.printNumbers(1,10);
-        BasicRecursion.printReverseNumber(10);
-        BasicRecursion.sumOfFirstNNaturalNumbers(5,0);
-        System.out.println(BasicRecursion.sumOfFirstNNaturalNumbers(5));
-        BasicRecursion.factorialParametrized(5,1);
-        System.out.println(BasicRecursion.factorialFunctional(5));
-        BasicRecursion.reverseArray(new int[]{1,2,3,4,5,6,7,8,9});
-        System.out.println(BasicRecursion.checkIfPalindrome("madam"));
-        System.out.println(BasicRecursion.fibonacci(6));
-        BasicRecursion.printFibonacci(5);
+
+        Map<String,Integer> map = new HashMap<>();
+        map.put("Apple",1);
+        map.put("Banana",2);
+        map.put("Orange",3);
+        map.put("Watermelon",4);
+        map.put("Pineapple",5);
+
+        System.out.println(map);
+        System.out.println(map.get("Apple"));
+        System.out.println(map.getOrDefault("Raspberry",0));
+        System.out.println(map);
+        System.out.println(map.values());
+        System.out.println(map.keySet());
+        for(var item: map.entrySet()){
+            System.out.println(item.getValue());
         }
+    }
+
 }
