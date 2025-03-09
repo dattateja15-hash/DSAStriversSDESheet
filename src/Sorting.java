bca80cf1dd4926a82ec65f111e231eee5844f615
Datta Teja, Nalluri <dattatejan@gmail.com>
2025-03-08 21:46:52 -0600

    Updated Bubble sort

--- src/Sorting.java
+++ src/Sorting.java
@@ -21,7 +21,8 @@ public class Sorting {
         }
 
         public static void bubbleSort(int[] arr){
-        for(int i = arr.length-1;i>=1;i--){
+        int n = arr.length;
+        for(int i = n-1;i>=1;i--){
             for(int j = 0;j<=i-1;j++){
                 if(arr[j]>arr[j+1]){
                     int temp = arr[j];
